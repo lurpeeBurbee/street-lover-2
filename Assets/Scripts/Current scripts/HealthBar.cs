1292d718a7897f8b2218984c2173c70d44227729
lurpeeBurbee <gosilvershine@msn.com>
2022-11-24 09:18:46 +0200

    Update HealthBar.cs

--- Assets/Scripts/Current scripts/HealthBar.cs
+++ Assets/Scripts/Current scripts/HealthBar.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
 
-    int HealthIndexGlobal = 100;
+   // int HealthIndexGlobal = 100;
+    int SuperPowerUp = 120;
 
     [SerializeField]
     List<GameObject> HealthBarList;
 
     void HealthBarManager(int AnyGlobalVariable)
     {
+
         AnyGlobalVariable /= 100; // Täysin sama kuin: AnyGlobalVariable = AnyGlobalVariable / 100
         Debug.Log("AnyGlobalVariable: " + AnyGlobalVariable);
 
+        //---- Manageroivat Healthbaria ainoastaan:
         int HealthIndexLocal = 0;
           HealthBarList[HealthIndexLocal].SetActive(false);
           HealthIndexLocal += 1;
@@ -24,12 +27,12 @@ public class HealthBar : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy")) 
         {
-            HealthBarManager(100); //<-- ajetaan täällä
+            HealthBarManager(SuperPowerUp); //<-- ajetaan täällä
         }
     }
     void Start()
     {
-   
+
     }
 
     void Update()
