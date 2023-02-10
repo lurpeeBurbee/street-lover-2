33cf988c1aa9dc603296a99b8713729a1d840c8c
lurpeeBurbee <53574763+lurpeeBurbee@users.noreply.github.com>
2023-02-10 10:21:01 +0200

    Updated scripts

---
+++ Assets/Scripts/Test scripts/StreetLover.cs
@@ -0,0 +1,52 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StreetLover : MonoBehaviour
+{
+    float MoveSpeed;
+
+    int PlayerHealth;
+    int MinHealth;
+
+    float RunSpeed;
+    bool IsOnAir;
+
+    void HealthManager()
+    {
+
+        if (PlayerHealth > MinHealth) // TRUE
+        { // Funktio alkaa
+            PlayerHealth -= 1;
+            Debug.Log(PlayerHealth);
+        }
+        if (PlayerHealth == 0) // FALSE
+        {
+            Debug.Log("Game over");
+            PlayerHealth -= 1;
+        }
+
+    } // -Funktio loppuu
+
+    void Start()
+    {
+        MoveSpeed = 0;
+        PlayerHealth = 100;
+        MinHealth = 0;
+        RunSpeed = 2;
+        IsOnAir = false;
+
+    }
+    void Update()
+    {
+
+     
+
+
+
+        //Debug.Log("MoveSpeed is " + MoveSpeed);
+        //print(PlayerHealth);
+    }
+}
