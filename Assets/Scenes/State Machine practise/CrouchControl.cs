201368ec57f21c470ced4624a344238456aeafa8
lurpeeBurbee <53574763+lurpeeBurbee@users.noreply.github.com>
2023-04-28 14:03:14 +0200

    Crouching state machine updates

--- Assets/Scenes/State Machine practise/CrouchControl.cs
+++ Assets/Scenes/State Machine practise/CrouchControl.cs
@@ -7,11 +7,36 @@ public class CrouchControl : MonoBehaviour
     public GameObject head;
     [SerializeField] Animator anim;
 
+    CapsuleCollider2D playerCollider;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Cave"))
         {
             anim.SetBool("Crouch", true);
+
+            var isCrouching = anim.GetBool("IsCrouching");
+            if(isCrouching)
+            {
+                playerCollider = GetComponent<CapsuleCollider2D>();
+                playerCollider.enabled = !isCrouching; 
+                Debug.Log("Is on crouching state");
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CaveExit"))
+        {
+            anim.SetBool("Crouch", false);
+
+            var isCrouching = anim.GetBool("IsCrouching");
+            if (isCrouching == false)
+            {
+                playerCollider = GetComponent<CapsuleCollider2D>();
+                playerCollider.enabled = true;
+                Debug.Log("Exiting crouching state");
+            }
         }
     }
 
