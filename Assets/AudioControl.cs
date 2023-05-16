df60105a75e83e69830a716c9d74ce9802a987f2
Lurppa <53574763+lurpeeBurbee@users.noreply.github.com>
2023-05-16 20:52:14 +0300

    UI settings update

---
+++ Assets/AudioControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControl : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void MuteSelectedAudio(AudioSource source) { // valitse inspectorista, minkä Audio Sourcen halua mutettaa
+    if(audioMixer != null)
+        {
+            if(source.isPlaying)
+            {
+                source.Stop();
+            }
+            else
+            {
+                source.Play();
+            }
+        }
+    }
+
+    public void MuteAudio(float audioVolume) {  
+        if(audioMixer != null)
+        {
+            if(audioMixer.GetFloat("volume", out audioVolume) && audioVolume > -80) 
+            {
+                audioMixer.SetFloat("volume", -80);
+            }
+        }
+    }
+    public void ControlAudio(float audioVolume)
+    {
+        audioMixer.SetFloat("volume", audioVolume);    
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Settings scripts/GraphicSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicSettings : MonoBehaviour
+{
+
+public void SetGraphicsQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);   
+    }
+}
