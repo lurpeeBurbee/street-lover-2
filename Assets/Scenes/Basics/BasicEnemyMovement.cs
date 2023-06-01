eca34a884341a8f9c47f4400078d1ddd24af7728
Lurppa <53574763+lurpeeBurbee@users.noreply.github.com>
2023-06-01 13:11:49 +0300

    Basic enemy movement pingpong

---
+++ Assets/Scenes/Basics/BasicEnemyMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BasicEnemyMovement : MonoBehaviour
+{
+    [SerializeField] float enemyMoveValue;
+    [SerializeField] float moveSpeed;
+    readonly float enemyMaxLeftPosition = -7.3f;
+    public bool canMoveLeft;
+    void MoveEnemy()
+    {
+        if (transform.position.x > enemyMaxLeftPosition && canMoveLeft == true) // toimii myös pelkkä canMoveLeft
+            // jos pitää checkata että on false, niin kirjoita !canMoveLeft
+        {
+            transform.position = new Vector2(enemyMoveValue -= moveSpeed * Time.deltaTime, transform.position.y);
+            if(transform.position.x <= -7.3) canMoveLeft = false;   
+
+        }
+        else
+        {
+            transform.position = new Vector2(enemyMoveValue += moveSpeed * Time.deltaTime, transform.position.y);
+            if (transform.position.x >= 7.3) canMoveLeft = true;
+        }
+        
+    }
+    void Start()
+    {
+        canMoveLeft = true;
+    }
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+
+}
