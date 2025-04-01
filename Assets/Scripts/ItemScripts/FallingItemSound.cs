98191145759fbcc8348a3025e614021cc96e657b
lattesprezzo <lattesprezzo@gmail.com>
2025-04-01 13:29:39 +0300

    Audio scene more

---
+++ Assets/Scripts/ItemScripts/FallingItemSound.cs
@@ -0,0 +1,49 @@
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FallingItemSound : MonoBehaviour
+{
+    [Header("Sound Settings")]
+    [Tooltip("The sound to play when the item hits the floor.")]
+    public AudioClip hitSound;
+    [Tooltip("The volume of the sound.")]
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Tooltip("The pitch of the sound.")]
+    [Range(0.1f, 3f)]
+    public float pitch = 1f;
+
+    [Header("Layer Settings")]
+    [Tooltip("The layer of the floor.")]
+    public LayerMask groundLayer;
+
+    private AudioSource audioSource;
+    private bool canPlaySound = true;
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = hitSound;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.playOnAwake = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0 && canPlaySound)
+        {
+            audioSource.Play();
+            canPlaySound = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            canPlaySound = true;
+        }
+    }
+}
